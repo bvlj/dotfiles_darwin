abf81bd998b43c927bac9c42d7756ea2572a0e34
Joey <bevilacquajoey@gmail.com>
2022-11-13 21:48:33 +0100

    git: per-repo identity
    Based on: https://pickard.cc/posts/git-identity-home-manager/

--- .nixpkgs/pkgs/git.nix
+++ .nixpkgs/pkgs/git.nix
@@ -4,6 +4,24 @@
     ".DS_Store"
     "*~"
   ];
-  userEmail = "bevilacquajoey@gmail.com";
-  userName = "joey";
+
+  extraConfig = {
+    user.useConfigOnly = true;
+
+    user.personal.name = "Joey";
+    user.personal.email = "bevilacquajoey@gmail.com";
+
+    user.usi.name = "Joey Bevilacqua";
+    user.usi.email = "bevilj@usi.ch";
+
+    user.lineage.name = "Joey";
+    user.lineage.email = "joey@lineageos.org";
+  };
+
+  aliases = {
+    id = "! git-identity";
+    identity = "! git-identity";
+    skip = "! git-skip";
+    unskip = "! git-unskip";
+  };
 }
